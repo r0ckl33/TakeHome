1ad8b9f151a137dcefca82d83525829bedb6e329
r0ckl33 <43154583+r0ckl33@users.noreply.github.com>
2019-09-11 11:24:13 -0400

    Changed the name of string to a slightly more meaningful name.

--- src/com/takehome/Main.java
+++ src/com/takehome/Main.java
@@ -10,32 +10,32 @@ public class Main {
     }
 
     /*
-    *  Print the transformation of stringOne into stringTwo one letter at a time.
-    *
-    *  Stores stringOne in a StringBuilder and then loop through each individual character
-    *  changing it to the corresponding positional character from stringTwo, then prints.
-    *
-    *  If the lengths of stringOne and stringTwo are not equal then the function will
-    *  return a friendly message and then exit.
-    *
-    *  @param  stringOne  a string to be transformed
-    *  @param  stringTwo  a string to be copied from
-    */
+     *  Print the transformation of stringOne into stringTwo one letter at a time.
+     *
+     *  Stores stringOne in a StringBuilder and then loop through each individual character
+     *  changing it to the corresponding positional character from stringTwo, then prints.
+     *
+     *  If the lengths of stringOne and stringTwo are not equal then the function will
+     *  return a friendly message and then exit.
+     *
+     *  @param  stringOne  a string to be transformed
+     *  @param  stringTwo  a string to be copied from
+     */
     private static void TransformString(String stringOne, String stringTwo) {
         if (stringOne.length() != stringTwo.length()){
             System.out.println("The lengths of 'stringOne' and 'stringTwo' must be equal.");
             return;
         }
 
-        StringBuilder string = new StringBuilder(stringOne);
+        StringBuilder newString = new StringBuilder(stringOne);
 
-        System.out.println(string);
+        System.out.println(newString);
 
-        for (int i = 0; i < string.length(); i++) {
+        for (int i = 0; i < newString.length(); i++) {
             // Update a character in the StringBuilder.
-            string.setCharAt(i, stringTwo.charAt(i));
+            newString.setCharAt(i, stringTwo.charAt(i));
 
-            System.out.println(string);
+            System.out.println(newString);
         }
     }
 
